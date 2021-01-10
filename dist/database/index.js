05e2664ff150e910660cefa02b8af5eae631b7cb
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-01-10 11:03:41 -0300

    config dotenv

--- dist/database/index.js
+++ dist/database/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
+const index_1 = require("../database/index");
+index_1.default;
 const database = mongoose.connect(process.env.MONGO, {
     useNewUrlParser: true, useUnifiedTopology: true
 });
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,6 @@
 import * as mongoose from 'mongoose';
+import Database from '../database/index';
+Database;
 
 const database = mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true, useUnifiedTopology: true
