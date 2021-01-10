2dc58be5729cc018a20f76eb82096bcc33117f40
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-01-10 11:06:57 -0300

    modify PORT

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./server/index");
-index_1.default.listen(process.env.PORT || 8081, () => {
-    console.log('run');
-});
+index_1.default.listen(process.env.PORT || 3000);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import App from './server/index';
 
-App.listen(process.env.PORT || 8081, () => {
-    console.log('run');  
-});
+App.listen(process.env.PORT || 3000);
