68a6efd97196d48abacc66fa79599f8c873d600f
Maicon Augusto <maicon.augusto.carvalho@hotmail.com>
2021-01-19 09:53:30 -0300

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./server/index");
-index_1.default.listen(process.env.PORT || 3000);
+index_1.default.listen(process.env.PORT);
